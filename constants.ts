80cf80bfaa9c100a12f8e8e626863b9bb95f7e29
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-05-02 21:58:50 -0700

    add constants and PageMetaData component

---
+++ constants.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_IMAGE_PATH = 'assets/og-splash.png'
+export const SITE_NAME = 'MetaCamp'
+export const SITE_URL = 'https://metacamp.community'
+export const TWITTER_HANDLE = '@metacampcommunity'
