cbb890a2d3afde1fa41ede195929e8b964abff8e
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-05-02 22:09:43 -0700

    update index as splash page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,61 @@
-import { Flex, Heading, IconButton, Text, useColorMode } from '@chakra-ui/react'
-import { SunIcon, MoonIcon } from '@chakra-ui/icons'
-import Head from 'next/head'
-import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { Box, Flex, Image } from '@chakra-ui/react'
+// import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { PageMetadata } from '@/components'
 
 export default function Home() {
-  const { colorMode, toggleColorMode } = useColorMode()
-  const isDarkMode = colorMode === 'dark'
   return (
-    <>
-      <Head>
-        <title>TITLE</title>
-        <meta name="description" content="DESCRIPTION" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <Flex
-          maxW="50ch"
-          direction="column"
-          mx="auto"
-          px="6"
-          py="8"
-          textAlign="center"
-          gap={4}
-          borderRadius="xl"
-          background="primary"
-          mt={8}
+    <Box bg="#F0B130" h="100vh" w="100vw">
+      <PageMetadata
+        title="MetaCamp"
+        description="An IRL community experience"
+      />
+      <Flex
+        as="main"
+        direction="column"
+        h="full"
+        align="center"
+        bg="#292F51"
+        pt="15vmin"
+      >
+        <Image
+          src="/assets/chili-home.svg"
+          position="absolute"
+          inset="1rem auto auto 1.5rem"
+          width="8vmin"
+          aspectRatio={1}
+          alt="Chili home icon"
+        />
+        <Image
+          src="/assets/costa-rica.svg"
+          width="clamp(100px, 40%, 290px)"
+          height="72px"
+          alt="Costa Rica text"
+          zIndex={2}
+        />
+        <Image
+          src="/assets/title-logo.svg"
+          width="clamp(200px, 90%, 800px)"
+          alt="MetaCamp logo"
+          zIndex={2}
+          px={8}
+        />
+        <Box
+          w="100%"
+          position="absolute"
+          inset="auto 0 0 0"
         >
-          <Heading as="h1">Hello world</Heading>
-          <Text>This is a NextJS + Chakra-UI + RainbowKit app template</Text>
-          <IconButton
-            w="fit-content"
-            mx="auto"
-            icon={isDarkMode ? <SunIcon /> : <MoonIcon />}
-            onClick={toggleColorMode}
-            aria-label="Toggle color mode"
+          <Image
+            src="/assets/hero-bg.svg"
+            objectFit="cover"
+            objectPosition="left"
+            minW="100%"
+            height="70vh"
+            alt="Beach background art"
+            zIndex={1}
+            overflow="visible"
           />
-          <ConnectButton />
-        </Flex>
-      </main>
-    </>
+        </Box>
+      </Flex>
+    </Box>
   )
 }
