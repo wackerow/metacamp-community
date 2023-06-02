21eb19a864fe83a55e5aa66a5e1a32f478cbad45
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-06-02 15:15:07 -0700

    fix image paths

--- src/constants.ts
+++ src/constants.ts
@@ -9,21 +9,21 @@ export const CAMPER_QUOTES: CamperQuote[] = [
   {
     author: 'Gloria Kexin Wu',
     association: 'Karma3Labs',
-    imageSrc: '/assets/campers/avatar-1.jpeg',
+    imageSrc: '/assets/campers/quotes/avatar-1.jpeg',
     quote:
       "MetaCamp is this magic place where you meet an alternative reality and you meet people who share the values to build an alternative reality, for the better of the world. I had such a fantastic time with my fellow campers to explore the latest thoughts and learnings of mechanism design for DAOs. I got lots of inspirations for my project's mission to build the right reputation mechanisms for decentralized systems. But most importantly, I met the right people for building together.",
   },
   {
     author: 'Kevin Owocki',
     association: 'SuperModular',
-    imageSrc: '/assets/campers/avatar-2.jpeg',
+    imageSrc: '/assets/campers/quotes/avatar-2.jpeg',
     quote:
       "Metacamp had the best of costa rica: sunshine, surf, hiking, & nautral beauty. But the real magic at is in the curated participants, which offered great mental stimulation as well - The unconference sessions allowed me to nerd out in paradise with my favorite DAO frens. The vibes were high at an all time high and I'll be back next year, because MetaCamp is 1337.",
   },
   {
     author: "Dermot O'Riordan",
     association: 'POKT Network',
-    imageSrc: '/assets/campers/avatar-3.jpeg',
+    imageSrc: '/assets/campers/quotes/avatar-3.jpeg',
     quote:
       'If culture is what you do, not what you say, then Metacamp’s culture is about sharing, supporting, and learning while having lots of fun and not taking yourself too seriously. The stewards have created a really special, warm and inclusive community/retreat that makes it easy for friendships to be forged for people to freely share their perspectives and problems in the knowledge that the rest of the group is on-hand to listen and help. 10/10.',
   },
